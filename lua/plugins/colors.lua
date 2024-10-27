05cd310ef5eaacefc52471b3896d8cd9bce31f30
Delta <92547844+pyxel00@users.noreply.github.com>
2024-10-27 22:59:00 +0300

    Update colors.lua

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -1,13 +1,5 @@
 return {
   {
     'catppuccin/nvim'
-  },
-  {
-    "baliestri/aura-theme",
-    lazy = false,
-    priority = 1000,
-    config = function(plugin)
-      vim.opt.rtp:append(plugin.dir .. "/packages/neovim")
-    end
   }
 }

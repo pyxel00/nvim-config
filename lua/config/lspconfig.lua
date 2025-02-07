846b9dca31b23bd6fc2cf2ff8ab606b282f21198
delta <pyxel1143@gmail.com>
2025-02-07 23:04:55 +0300

    Update

--- lua/config/lspconfig.lua
+++ lua/config/lspconfig.lua
@@ -9,20 +9,14 @@ local on_attach = function(_, bufnr)
   vim.lsp.inlay_hint.enable(true)
 end
 
-lspconfig.rust_analyzer.setup{
-  on_attach = on_attach,
-  capabilities = capabilities,
-  filetypes = { "rust" }
-}
-
 lspconfig.clangd.setup{
   on_attach = on_attach,
   capabilities = capabilities,
   filetypes = { "c", "cpp" }
 }
 
-lspconfig.jdtls.setup{
+lspconfig.gopls.setup{
   on_attach = on_attach,
   capabilities = capabilities,
-  filetypes = { "java" }
+  filetypes = { "go" }
 }
